fce1c4a25711cdacf386d95e7c484e015d1eb3e2
Artem Aminov <artemaminov@gmail.com>
2012-08-11 21:21:48 +0600

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Soft72Local::Application.config.secret_token = 'cc3b42db5c020dfc43a923a23c51acf783ba3558cbf391e0210e3da70ba4b1fb24f4c0c4d8dd1cae95531cf4d7d7b413b5213ba0e4c3a55a721083d462202560'
