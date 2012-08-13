9d86e2777534eef30c4713c7a8f511af5d29bfc2
Artem Aminov <artemaminov@gmail.com>
2012-08-13 22:42:53 +0600

    Deface bug fixed

--- app/overrides/products_import_tab.rb
+++ app/overrides/products_import_tab.rb
@@ -2,6 +2,6 @@ Deface::Override.new(
 	:name => "products_import_tab",
 	:virtual_path => "spree/layouts/admin",
 	:insert_bottom => "[data-hook='admin_tabs'], #admin_tabs[data-hook]",
-	:text => 'Import products'
+	:text => '<li><a href="/admin/promotions">Import products</a></li>'
 	# :text => '<%= tab :products_import %>'
 	)
